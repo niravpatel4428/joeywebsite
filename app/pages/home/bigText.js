71de23202fce7c3f19adb094afc05479f919180d
yashzindex <yashzindex@gmail.com>
2025-08-05 18:12:51 +0530

    small change in bigtext

--- app/pages/home/bigText.js
+++ app/pages/home/bigText.js
@@ -3,7 +3,7 @@ import React from "react";
 const BigText = () => {
   return (
     <section className="relative overflow-hidden bg-foreground py-5 xl:py-12">
-      <h4 className="text-background max-[390px]:text-2xl text-28 sm:text-5xl md:text-[61px] lg:text-[80px] xl:text-140 font-semibold tracking-low sm:tracking-thin leading-none whitespace-nowrap text-center">
+      <h4 className="text-background max-[390px]:text-2xl text-28 sm:text-5xl md:text-[61px] lg:text-[80px] xl:text-[96px] font-semibold tracking-low sm:tracking-thin leading-none whitespace-nowrap text-center">
         Agent layer task intelligence
       </h4>
     </section>
