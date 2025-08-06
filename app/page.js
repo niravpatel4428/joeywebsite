8204a648ce59eb490c8a02ef2da6ff0590667b5b
Your Nameniravpatel4428 <nirav.jivani6@gmail.com>
2025-08-06 08:49:16 +0530

    home page change

--- app/page.js
+++ app/page.js
@@ -11,12 +11,12 @@ export default function Home() {
       <Agents />
       <Experties />
       <BigText />
-      <Testimonials
+      {/* <Testimonials
         title="What Our Clients Say"
         description="“Joey is a remarkable artist. He grasps abstract ideas and transforms them into exceptional visuals. Throughout the years, I've witnessed him produce everything from small symbols to comprehensive adaptive web pages. He has crafted clothing items like tees and caps, promotional products, and professional literature.”"
         authorName="Chris Ellington"
         authorRole="Product Lead,Gatha."
-      />
+      /> */}
     </>
   );
 }
